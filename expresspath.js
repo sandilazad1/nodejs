faffe0707fa7006594044df91ccaec728efeb3e2
sandil azad <sandilazad@gmail.com>
2021-07-03 15:16:23 +0600

    express server path

--- expresspath.js
+++ expresspath.js
@@ -1,14 +1,28 @@
 const http = require('http');
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
-// please use / path in last on the code 
-app.use('/add',(req,res,next)=>{
-    res.send('request send for add path');
-    })
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/',(req,res,next)=>{
-res.send('request send for / path');
+
+app.use('/add-product', (req, res, next) => {
+    res.send(`<form action="/product" method="POST"><label for="fname">First name:</label>
+    <input type="text" id="fname" name="fname"><br><br>
+    <label for="lname">Last name:</label>
+    <input type="text" id="lname" name="lname"><br><br>
+    <input type="submit" value="Submit">
+    </form>`);
 })
 
-const server = http.createServer(app);
-server.listen(3000)
+
+
+app.use('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+});
+
+app.use('/', (req, res, next) => {
+    res.send('request send for / path');
+})
+
+app.listen(3000);
